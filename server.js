aecadc32d692bf36f39f2084f5f9d100ffed235a
Dwima Basumatary <dwima1583@gmail.com>
2023-03-20 00:28:47 +0530

    nothing

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        secure: true,
+        secure: false,
         maxAge: 1000*60*60*24
     },
     store: MongoStore.create({mongoUrl: process.env.DATABASE_URL, collectionName: 'sessions'})
